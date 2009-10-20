d5a28b8bfee9f1bbec3a7f9f474c8a0625d61700
Untoldwind <landru@untoldwind.net>
2009-10-20 18:06:27 +0200

    Pared generator node collapse

--- ui/base/src/net/untoldwind/moredread/ui/actions/GeneratorNodeActionDelegate.java
+++ ui/base/src/net/untoldwind/moredread/ui/actions/GeneratorNodeActionDelegate.java
@@ -1,6 +1,12 @@
 package net.untoldwind.moredread.ui.actions;
 
+import net.untoldwind.moredread.model.mesh.IMesh;
+import net.untoldwind.moredread.model.mesh.Mesh;
+import net.untoldwind.moredread.model.scene.AbstractSpatialComposite;
+import net.untoldwind.moredread.model.scene.GeneratorNode;
 import net.untoldwind.moredread.model.scene.INode;
+import net.untoldwind.moredread.model.scene.MeshNode;
+import net.untoldwind.moredread.model.scene.SceneChangeHandler;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -13,20 +19,33 @@ public class GeneratorNodeActionDelegate implements IObjectActionDelegate {
 	INode selectedNode;
 
 	@Override
-	public void setActivePart(final IAction action,
-			final IWorkbenchPart targetPart) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void run(final IAction action) {
 		final String id = action.getId();
 
 		if ("net.untoldwind.moredread.ui.generatorNode.collapse".equals(id)) {
-			System.out.println(">> " + selectedNode);
-			System.out.println(">>> " + action);
+			System.out.println(">> dodo");
+			if (selectedNode != null && selectedNode instanceof GeneratorNode) {
+				System.out.println(">> dodo2");
+				final IMesh mesh = ((GeneratorNode) selectedNode)
+						.getRenderGeometry();
 
+				if (mesh instanceof Mesh<?>) {
+					System.out.println(">> dodo3");
+					final SceneChangeHandler sceneChangeHandler = selectedNode
+							.getScene().getSceneChangeHandler();
+
+					sceneChangeHandler.begin(true);
+					try {
+						final AbstractSpatialComposite<?> parent = ((GeneratorNode) selectedNode)
+								.getParent();
+						selectedNode.remove();
+
+						new MeshNode(parent, (Mesh<?>) mesh);
+					} finally {
+						sceneChangeHandler.commit();
+					}
+				}
+			}
 		}
 	}
 
@@ -46,4 +65,8 @@ public class GeneratorNodeActionDelegate implements IObjectActionDelegate {
 		}
 	}
 
+	@Override
+	public void setActivePart(final IAction action,
+			final IWorkbenchPart targetPart) {
+	}
 }
