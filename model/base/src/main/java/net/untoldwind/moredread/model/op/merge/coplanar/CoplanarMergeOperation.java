ace37b8eae060df4a40200e439c0404fa93a4c40
Untoldwind <landru@untoldwind.net>
2011-09-10 17:04:22 +0200

    Add some doc

--- model/base/src/main/java/net/untoldwind/moredread/model/op/merge/coplanar/CoplanarMergeOperation.java
+++ model/base/src/main/java/net/untoldwind/moredread/model/op/merge/coplanar/CoplanarMergeOperation.java
@@ -22,6 +22,12 @@ import com.jme.math.FastMath;
 import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
 
+/**
+ * Simple coplanar merge operation.
+ * 
+ * Merges all coplanar faces of a mesh to single faces. Also remove unnecessary
+ * vertices.
+ */
 public class CoplanarMergeOperation implements IUnaryOperation {
 	@Override
 	public Mesh<?> perform(final IMesh mesh) {
@@ -29,6 +35,7 @@ public class CoplanarMergeOperation implements IUnaryOperation {
 		final List<Integer> unmergableFaces = new ArrayList<Integer>();
 		final List<Set<Integer>> facesToMerge = new ArrayList<Set<Integer>>();
 
+		// Find all faces that can be combined
 		for (final IFace face : mesh.getFaces()) {
 			if (visitedFaces.contains(face.getIndex())) {
 				continue;
@@ -66,6 +73,18 @@ public class CoplanarMergeOperation implements IUnaryOperation {
 		return result;
 	}
 
+	/**
+	 * Simple transfer a face of the source mesh to the target mesh.
+	 * 
+	 * @param target
+	 *            The target mesh
+	 * @param source
+	 *            The source mesh
+	 * @param faceIndex
+	 *            The index to the face to transfer
+	 * @param vertexMap
+	 *            The vertex map (source index -> target index)
+	 */
 	private void transferFace(final PolyMesh target, final IMesh source,
 			final int faceIndex, final Map<Integer, Integer> vertexMap) {
 		final IFace face = source.getFace(faceIndex);
@@ -93,10 +112,23 @@ public class CoplanarMergeOperation implements IUnaryOperation {
 		target.addFace(targetStrips);
 	}
 
+	/**
+	 * Merge faces of the source mesh and add combined face to target mesh.
+	 * 
+	 * @param target
+	 *            The target mesh
+	 * @param source
+	 *            The source mesh
+	 * @param faces
+	 *            The indices of all face to merge
+	 * @param vertexMap
+	 *            The vertex map (source index -> target index)
+	 */
 	private void mergeFaces(final PolyMesh target, final IMesh source,
 			final Set<Integer> faces, final Map<Integer, Integer> vertexMap) {
 		final Map<Integer, List<Integer>> borderEdges = new HashMap<Integer, List<Integer>>();
 
+		// Collect the border edges (i.e. the edges of the combined face
 		for (final int faceIndex : faces) {
 			final IFace face = source.getFace(faceIndex);
 
